6b554103647a644ff4be438c11d259cbd4e340eb
Hoel Chatellier <hoel.chatellier@bappli.com>
2013-02-05 11:52:27 +0100

    Fix buttons

--- wiki/controllers/Page_Output_Controller.php
+++ wiki/controllers/Page_Output_Controller.php
@@ -1,6 +1,8 @@
 <?php
 namespace SAF\Wiki;
 use SAF\Framework\Controller_Parameters;
+use SAF\Framework\Button;
+use SAF\Framework\View;
 use SAF\Framework\Default_Output_Controller;
 
 class Page_Output_Controller extends Default_Output_Controller
@@ -8,20 +10,11 @@ class Page_Output_Controller extends Default_Output_Controller
 
 	protected function getGeneralButtons($object)
 	{
+		$destination = "#main";
+		if($object->name == "Menu")
+			$destination = "#menu";
 		return Button::newCollection(array(
-			array("Edit", View::link($object, "edit"), "edit", array(Color::of("green"), "#main"))
+			array("Edit", View::link($object, "edit"), "edit", array($destination))
 		));
-	}
-
-
-	protected function getViewParameters(Controller_Parameters $parameters, $class_name)
-	{
-		$parameters = parent::getViewParameters($parameters, $class_name);
-		if($_SERVER["PATH_INFO"] && $_SERVER["PATH_INFO"] != "new"){
-			$object = $parameters[$class_name];
-			$object->name = str_replace("_", " ", str_replace("/", "", $_SERVER["PATH_INFO"]));
-		}
-
-		return $parameters;
 	}
 }
